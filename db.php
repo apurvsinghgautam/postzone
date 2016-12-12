d4343485f1f512b3d1874110d61a67f5a2bb0fbc
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2016-12-12 09:47:01 +0530

    Add files via upload

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername="sql6.freemysqlhosting.net";
-$username="sql6144371";
-$password="9NRZb9cvD1";
-$db_name="sql6144371";
+$username="sql6148986";
+$password="dTIg8HKfaC";
+$db_name="sql6148986";
 
 $conn=mysqli_connect($servername,$username,$password,$db_name);
 
